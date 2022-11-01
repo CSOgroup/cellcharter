41dac3e9262845dbac2c0ba399bdbc02b1eb163d
Marco Varrone <marco.varrone@unil.ch>
2022-11-01 21:48:40 +0100

    Setup tests

---
+++ tests/conftest.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pytest
+import scanpy as sc
+from anndata import AnnData
+from squidpy._constants._pkg_constants import Key
+
+_adata = sc.read("tests/_data/test_data.h5ad")
+_adata.raw = _adata.copy()
+
+
+@pytest.fixture()
+def non_visium_adata():
+    non_visium_coords = np.array([[1, 0], [3, 0], [5, 6], [0, 4]])
+    adata = AnnData(X=non_visium_coords, dtype=int)
+    adata.obsm[Key.obsm.spatial] = non_visium_coords
+    return adata
