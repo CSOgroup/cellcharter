63105986f140f309e2f88363e28ced6493b4f619
Marco Varrone <marco.varrone@unil.ch>
2022-11-01 20:56:57 +0100

    Replace pp with gr

--- src/cellcharter/__init__.py
+++ src/cellcharter/__init__.py
@@ -1,7 +1,7 @@
 from importlib.metadata import version
 
-from . import pl, pp, tl
+from . import gr, pl, tl
 
-__all__ = ["pl", "pp", "tl"]
+__all__ = ["gr", "pl", "tl"]
 
 __version__ = version("cellcharter")
