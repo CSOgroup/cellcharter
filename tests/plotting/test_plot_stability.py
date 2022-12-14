e5fa77cf92811bc5bfda3a0c66f71cb4f5467084
Marco Varrone <mrc.varrone@gmail.com>
2022-12-14 14:20:49 +0100

    Avoid fitting ClusterAutoK in plot_stability test

---
+++ tests/plotting/test_plot_stability.py
@@ -0,0 +1,21 @@
+import pytest
+import scipy.sparse as sps
+import squidpy as sq
+
+import cellcharter as cc
+
+
+class TestPlotStability:
+    @pytest.mark.parametrize("dataset_name", ["imc", "mibitof"])
+    def test_spatial_proteomics(self, dataset_name):
+        download_dataset = getattr(sq.datasets, dataset_name)
+        adata = download_dataset()
+        if sps.issparse(adata.X):
+            adata.X = adata.X.todense()
+        sq.gr.spatial_neighbors(adata, coord_type="generic", delaunay=True)
+        cc.gr.remove_long_links(adata)
+        cc.gr.aggregate_neighbors(adata, n_layers=3)
+
+        model = cc.tl.ClusterAutoK.load(f"tests/_models/cellcharter_autok_{dataset_name}")
+
+        cc.pl.plot_stability(model)
