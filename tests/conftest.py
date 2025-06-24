988ff8507202a4007c56f5100ca552bde61b9198
Marco Varrone <marco.varrone@unil.ch>
2025-06-24 09:44:09 +0200

    Fix obs names warning in test dataset

--- tests/conftest.py
+++ tests/conftest.py
@@ -24,4 +24,5 @@ def adata() -> ad.AnnData:
 @pytest.fixture(scope="session")
 def codex_adata() -> ad.AnnData:
     adata = sc.read("tests/_data/codex_adata.h5ad", backup_url="https://figshare.com/ndownloader/files/46832722")
+    adata.obs_names_make_unique()
     return adata[adata.obs["sample"].isin(["BALBc-1", "MRL-5"])].copy()
